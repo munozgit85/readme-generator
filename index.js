aa3e394101b4ad6c958ae44aba023bbf1ab203df
munozgit85 <107218022+munozgit85@users.noreply.github.com>
2022-09-09 14:35:53 -0500

    corrected the file path

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 // TODO: Include packages needed for this application
 const inquirer = require('inquirer');
 const fs = require('fs');
-const { generateMarkdown } = require('../readme-generator/utils/generateMarkdown');
+const generateMarkdown  = require('../readme-generator/utils/generateMarkdown');
 // TODO: Create an array of questions for user input
 const questions = () => {
     return inquirer.prompt([
@@ -133,7 +133,7 @@ function init() {
             return generateMarkdown(input);
         })
         .then(markdown => {
-            writeToFile('./Develop/README.md', markdown);
+            writeToFile('../dist/README.md', markdown);
         })
         .catch(err => {
             console.log(err);
